4e52c00889852b20c37054c6faa30d275cfd4cd2
Keldowin <ipeta2703@gmail.com>
2024-01-12 19:14:14 +0300

    Add docs (code and readme

--- ExtendTimer/etimer.gd
+++ ExtendTimer/etimer.gd
@@ -2,21 +2,33 @@
 class_name ETimer extends Node
 #Main ExtendTimer script file
 #Made by KELDOWIN
+#Version: 1.0
 
+##Process timer (Process, Physics)
 @export_enum("Idle","Physics") var ProcessCallback : int
+##Count when timer end (seconds)
 @export_range(1, 4096, 1) var WaitTime : int = 1
+##Delet when end
 @export var OneShot : bool = false
+##Start auto when init scene and timer end
 @export var AutoStart : bool = false
 
+#When timer end
 signal TimeOut
-signal OnEverySecond
+#Every second
+signal EverySecond
 
+#When timer stop
 signal TimerStop
+#When timer start
 signal TimerStart
 
+##Local varible, DeltaTime += delta
 var DeltaTime : float = 0.0
+##Time left (TimeLeft = WaitTime)
 @onready var TimeLeft : int = WaitTime
 
+## init timer
 func _ready() -> void:
 	set_process(false)
 	set_physics_process(false)
@@ -24,19 +36,23 @@ func _ready() -> void:
 	if AutoStart:
 		TimerProcess(true)
 
+##Start timer with setting process
 func TimerProcess(Process:bool) -> void:
 	match ProcessCallback:
 		0:
 			set_process(Process)
 		1:
 			set_physics_process(Process)
-			
+
+#PROCESS
 func _process(delta:float) -> void:
 	TimerFunc(delta)
 	
 func _physics_process(delta:float) -> void:
 	TimerFunc(delta)
+#PROCESS
 
+#update timer function
 func TimerFunc(delta:float) -> void:
 	DeltaTime += delta
 	
@@ -44,7 +60,7 @@ func TimerFunc(delta:float) -> void:
 		DeltaTime = 0.0
 		TimeLeft -= 1
 		
-		OnEverySecond.emit()
+		EverySecond.emit()
 		
 		#Timer end
 		if TimeLeft <= 0:
@@ -60,12 +76,14 @@ func TimerFunc(delta:float) -> void:
 			
 			if AutoStart:
 				TimerProcess(true)
-			
+
+##Stop timer
 func stop() -> void:
 	TimerProcess(false)
 	
 	TimerStop.emit()
-	
+
+##Start timer
 func start() -> void:
 	TimerProcess(true)
 	
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ExtendTimer for [Godot Engine](https://godotengine.org/) 4.x
 Godot 4.x plugin. Adds an improved timer with additional signals
+
+## 📂 Installation
+1. Download **ExtendTimer** addon.
+2. Extract the `ExtendTimer` folder into the `addons` folder within your Godot project directory.
+3. Enable the addon in Godot's plugins tab. (`Project > Project Settings > Plugins`)
+
+## 📃 Usage
+* Add the `ETimer` node to your scene(s) like any regular node in Godot.
+* Adjust the wait time values to your liking through the inspector tab.
+* Start the `ETimer` by calling the `$ETimer.start()` function or by enabling **autostart** in the inspector tab.
+
+## 🔢 Varibles and Signals
+### _Varibles_
+* `emun ProcessCallback : int = 0` - Timer update method ( _process(), _physic_process() )
+* `range(1, 4096) WaitTime : int = 1` - After how many seconds does the timer end.
+* `export OneShot : bool = false` - Whether to **delete** the timer after the end.
+* `export AutoStart : bool = false` - Whether to start the timer **automatically** at the end (and start of the scene)
+* `onready TimeLeft : int = 0` - A local variable used to calculate the remaining time (TimeLeft = WaitTime). 
+
+### _Signals_
+* `TimeOut` - The end of the timer
+* `EverySecond` - Each subtracting second from the timer
+* `TimerStop` - Timer stop use by function `$ETimer.stop()`
+* `TimerStart` - Timer start use by function `$ETimer.start()`
+
+---
+Credit by Keldowin for Godot Engine 💙
+Timer icon use from Godot Engine
